0c2f461c01814b12dd3cb5fd08820cc4587eab20
Sindre Østrem <sindre.ostrem@nb.no>
2023-11-06 14:57:06 +0100

    feat: TT-1222 Add simple file listing functionality from a folder

---
+++ src/lib/Files.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { readDir, BaseDirectory, type FileEntry } from '@tauri-apps/api/fs'
+  import { onMount } from 'svelte';
+
+  let files: FileEntry[] = []
+
+  onMount(async () => {
+    files = await readDir('trokk/files', {dir: BaseDirectory.Document})
+  })
+
+</script>
+
+<div class="files">
+  {#each files as file}
+    <p>{file.name}</p>
+  {/each}
+</div>
+
+<style lang="scss">
+  .files {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
