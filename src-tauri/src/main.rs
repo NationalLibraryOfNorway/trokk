f6aeafb2cecd48c8975e20232171b51196a13f4c
Marius Levang <marius.levang@nb.no>
2023-11-15 09:31:08 +0100

    Add hostname as scanner, simplify font and language

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,19 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use std::ffi::OsString;
+
+use gethostname::gethostname;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn get_hostname() -> Result<String, OsString> {
+    return gethostname().into_string()
 }
 
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_store::Builder::default().build())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_hostname])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
