ffe82c702d0be9e2aab255206126f74320fad456
Marius Levang <marius.levang@nb.no>
2023-11-15 14:54:32 +0100

    Make devtools start on startup

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,6 +4,7 @@
 use std::ffi::OsString;
 
 use gethostname::gethostname;
+use tauri::Manager;
 
 #[tauri::command]
 fn get_hostname() -> Result<String, OsString> {
@@ -13,6 +14,11 @@ fn get_hostname() -> Result<String, OsString> {
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_store::Builder::default().build())
+        .setup(|app| {
+            #[cfg(debug_assertions)]
+            app.get_window("main").unwrap().open_devtools(); // `main` is the first window from tauri.conf.json without an explicit label
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![get_hostname])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
