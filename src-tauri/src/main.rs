8657ae4c0f97138641328281281d7c78654dc5a2
Sindre Østrem <sindre.ostrem@nb.no>
2023-11-22 12:09:56 +0100

    feat: rustfmt and set indentations to tabs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,54 +1,59 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod model;
 mod auth;
+mod model;
 
 use crate::model::{AuthenticationResponse, RequiredEnvironmentVariables};
-use std::ffi::OsString;
-use once_cell::sync::Lazy;
 use gethostname::gethostname;
+use once_cell::sync::Lazy;
+use std::ffi::OsString;
 use tauri::Manager;
-use tauri::{Window};
+use tauri::Window;
 
-pub static ENVIRONMENT_VARIABLES: Lazy<RequiredEnvironmentVariables> = Lazy::new(|| RequiredEnvironmentVariables {
-    papi_path: env!("PAPI_PATH").parse().unwrap(),
-    oidc_base_url: env!("OIDC_BASE_URL").parse().unwrap(),
-    oidc_client_id: env!("OIDC_CLIENT_ID").parse().unwrap(),
-    oidc_client_secret: env!("OIDC_CLIENT_SECRET").parse().unwrap(),
-});
-
+pub static ENVIRONMENT_VARIABLES: Lazy<RequiredEnvironmentVariables> =
+	Lazy::new(|| RequiredEnvironmentVariables {
+		papi_path: env!("PAPI_PATH").parse().unwrap(),
+		oidc_base_url: env!("OIDC_BASE_URL").parse().unwrap(),
+		oidc_client_id: env!("OIDC_CLIENT_ID").parse().unwrap(),
+		oidc_client_secret: env!("OIDC_CLIENT_SECRET").parse().unwrap(),
+	});
 
 #[tauri::command]
 fn get_hostname() -> Result<String, OsString> {
-    return gethostname().into_string()
+	return gethostname().into_string();
 }
 
 #[tauri::command]
 fn get_required_env_variables() -> RequiredEnvironmentVariables {
-    ENVIRONMENT_VARIABLES.clone()
+	ENVIRONMENT_VARIABLES.clone()
 }
 
 #[tauri::command]
 async fn log_in(window: Window) -> Result<u16, String> {
-    // Returns the token via the `token_exchanged` window event
-    auth::log_in_with_server_redirect(window)
+	// Returns the token via the `token_exchanged` window event
+	auth::log_in_with_server_redirect(window)
 }
 
 #[tauri::command]
 async fn refresh_token(refresh_token: String) -> AuthenticationResponse {
-    auth::refresh_token(refresh_token).await
+	auth::refresh_token(refresh_token).await
 }
 
 fn main() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_store::Builder::default().build())
-        .setup(|app| {
-            #[cfg(debug_assertions)]
-            app.get_window("main").unwrap().open_devtools(); // `main` is the first window from tauri.conf.json without an explicit label
-            Ok(())
-        })
-        .invoke_handler(tauri::generate_handler![get_hostname, get_required_env_variables, log_in, refresh_token])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+	tauri::Builder::default()
+		.plugin(tauri_plugin_store::Builder::default().build())
+		.setup(|app| {
+			#[cfg(debug_assertions)]
+			app.get_window("main").unwrap().open_devtools(); // `main` is the first window from tauri.conf.json without an explicit label
+			Ok(())
+		})
+		.invoke_handler(tauri::generate_handler![
+			get_hostname,
+			get_required_env_variables,
+			log_in,
+			refresh_token
+		])
+		.run(tauri::generate_context!())
+		.expect("error while running tauri application");
 }
--- src-tauri/src/model.rs
+++ src-tauri/src/model.rs
@@ -3,56 +3,57 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Deserialize, Serialize, Clone)]
 #[serde(rename_all = "camelCase")]
 pub(crate) struct AuthenticationResponse {
-    pub(crate) token_response: TokenResponse,
-    pub(crate) expire_info: ExpireInfo,
-    pub(crate) user_info: UserInfo,
+	pub(crate) token_response: TokenResponse,
+	pub(crate) expire_info: ExpireInfo,
+	pub(crate) user_info: UserInfo,
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub(crate) struct TokenResponse {
-    #[serde(rename(serialize = "accessToken", deserialize = "access_token"))]
-    pub(crate) access_token: String,
-    #[serde(rename(serialize = "expiresIn", deserialize = "expires_in"))]
-    pub(crate) expires_in: i32,
-    #[serde(rename(serialize = "refreshExpiresIn", deserialize = "refresh_expires_in"))]
-    pub(crate) refresh_expires_in: i32,
-    #[serde(rename(serialize = "refreshToken", deserialize = "refresh_token"))]
-    refresh_token: String,
-    #[serde(rename(serialize = "tokenType", deserialize = "token_type"))]
-    token_type: String,
-    #[serde(rename(serialize = "notBeforePolicy", deserialize = "not-before-policy"))] // NB! Special kebab-case from OIDC-server
-    not_before_policy: i32,
-    #[serde(rename(serialize = "sessionState", deserialize = "session_state"))]
-    session_state: String,
-    scope: String,
+	#[serde(rename(serialize = "accessToken", deserialize = "access_token"))]
+	pub(crate) access_token: String,
+	#[serde(rename(serialize = "expiresIn", deserialize = "expires_in"))]
+	pub(crate) expires_in: i32,
+	#[serde(rename(serialize = "refreshExpiresIn", deserialize = "refresh_expires_in"))]
+	pub(crate) refresh_expires_in: i32,
+	#[serde(rename(serialize = "refreshToken", deserialize = "refresh_token"))]
+	refresh_token: String,
+	#[serde(rename(serialize = "tokenType", deserialize = "token_type"))]
+	token_type: String,
+	#[serde(rename(serialize = "notBeforePolicy", deserialize = "not-before-policy"))]
+	// NB! Special kebab-case from OIDC-server
+	not_before_policy: i32,
+	#[serde(rename(serialize = "sessionState", deserialize = "session_state"))]
+	session_state: String,
+	scope: String,
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 #[serde(rename_all = "camelCase")]
 pub(crate) struct ExpireInfo {
-    pub(crate) expires_at: u128,
-    pub(crate) refresh_expires_at: u128,
+	pub(crate) expires_at: u128,
+	pub(crate) refresh_expires_at: u128,
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub(crate) struct UserInfo {
-    sub: String,
-    name: String,
-    groups: Vec<String>,
-    #[serde(rename(serialize = "preferredUsername", deserialize = "preferred_username"))]
-    preferred_username: String,
-    #[serde(rename(serialize = "givenName", deserialize = "given_name"))]
-    given_name: String,
-    #[serde(rename(serialize = "familyName", deserialize = "family_name"))]
-    family_name: String,
-    email: String,
+	sub: String,
+	name: String,
+	groups: Vec<String>,
+	#[serde(rename(serialize = "preferredUsername", deserialize = "preferred_username"))]
+	preferred_username: String,
+	#[serde(rename(serialize = "givenName", deserialize = "given_name"))]
+	given_name: String,
+	#[serde(rename(serialize = "familyName", deserialize = "family_name"))]
+	family_name: String,
+	email: String,
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct RequiredEnvironmentVariables {
-    pub(crate) papi_path: String,
-    pub(crate) oidc_base_url: String,
-    pub(crate) oidc_client_id: String,
-    pub(crate) oidc_client_secret: String,
+	pub(crate) papi_path: String,
+	pub(crate) oidc_base_url: String,
+	pub(crate) oidc_client_id: String,
+	pub(crate) oidc_client_secret: String,
 }
