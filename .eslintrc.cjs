97ded28fa4595385e586ab51a85943610181573e
Sindre Østrem <sindre.ostrem@nb.no>
2023-11-22 13:09:19 +0100

    add eslint with tabs set

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+    extends: [
+        // add more generic rule sets here, such as:
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:svelte/recommended'
+    ],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    ignorePatterns: ['*.cjs'],
+    rules: {
+        // override/add rules settings here, such as:
+        // 'svelte/rule-name': 'error'
+        'svelte/indent': [
+            'error',
+            {
+                'indent': "tab"
+            }
+        ]
+    },
+    overrides: [
+        {
+            files: ['*.svelte'],
+            parser: 'svelte-eslint-parser',
+            parserOptions: {
+                parser: '@typescript-eslint/parser'
+            }
+        }
+    ]
+};
