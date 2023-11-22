39adf7e064f95ca5181fb80808a47d7c02f73c5d
Sindre Østrem <sindre.ostrem@nb.no>
2023-11-22 15:44:23 +0100

    remove prettier, use standard

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,31 +1,26 @@
 module.exports = {
-	extends: [
-		// add more generic rule sets here, such as:
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:svelte/recommended",
-		"plugin:svelte/prettier",
-	],
-	parser: "@typescript-eslint/parser",
-	plugins: ["@typescript-eslint"],
-	ignorePatterns: ["*.cjs"],
-	rules: {
-		// override/add rules settings here, such as:
-		// 'svelte/rule-name': 'error'
-		"svelte/indent": [
-			"error",
-			{
-				indent: "tab",
-			},
-		],
-	},
-	overrides: [
-		{
-			files: ["*.svelte"],
-			parser: "svelte-eslint-parser",
-			parserOptions: {
-				parser: "@typescript-eslint/parser",
-			},
-		},
-	],
+  extends: [
+    // add more generic rule sets here, such as:
+    // "eslint:recommended",
+    //"plugin:@typescript-eslint/recommended",
+    "plugin:svelte/recommended",
+    "plugin:svelte/prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  ignorePatterns: ["*.cjs"],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'svelte/rule-name': 'error'
+    "svelte/no-at-html-tags": "off",
+  },
+  overrides: [
+    {
+      files: ["*.svelte"],
+      parser: "svelte-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+      },
+    },
+  ],
 };
