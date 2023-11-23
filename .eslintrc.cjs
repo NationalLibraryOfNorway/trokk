e5d60ce608c4935d9c56901570a046dbcb4807b4
Sindre Østrem <sindre.ostrem@nb.no>
2023-11-23 11:43:10 +0100

    Remove comments from eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,11 @@
 module.exports = {
   extends: [
-    // add more generic rule sets here, such as:
     "plugin:svelte/recommended",
   ],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   ignorePatterns: ["*.cjs"],
   rules: {
-    // override/add rules settings here, such as:
-    // 'svelte/rule-name': 'error'
     "svelte/no-at-html-tags": "off",
   },
   overrides: [
