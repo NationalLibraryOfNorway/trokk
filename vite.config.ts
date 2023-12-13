46b2b50b15ce4da0c317333620eeff28df41f26a
Marius Levang <marius.levang@nb.no>
2023-12-13 14:05:47 +0100

    Add testing library and jsdom, and set up environment for testing svelte components

--- vite.config.ts
+++ vite.config.ts
@@ -26,4 +26,7 @@ export default defineConfig(async () => ({
   // 3. to make use of `TAURI_DEBUG` and other env variables
   // https://tauri.app/v1/api/config#buildconfig.beforedevcommand
   envPrefix: ["VITE_", "TAURI_"],
+  test: {
+   environment: 'jsdom'
+  }
 }));
