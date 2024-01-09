56e32e78c0aaf1498d31b73c71245b476cd33da5
Fredrik Monsen <fredrik.monsen@nb.no>
2024-01-09 15:02:19 +0100

    add allow to line that clippy throws a fit over

--- src-tauri/src/error/mod.rs
+++ src-tauri/src/error/mod.rs
@@ -1,2 +1,5 @@
-mod image_conversion_error;
+// WebPEncodingErrorWrapper is marked by clippy as unused import but it is used in the tests
+#[allow(unused_imports)]
 pub use image_conversion_error::{ImageConversionError, WebPEncodingErrorWrapper};
+
+mod image_conversion_error;
