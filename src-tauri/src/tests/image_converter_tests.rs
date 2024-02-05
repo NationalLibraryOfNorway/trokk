2b906fc3b0359d4193c0541a6a48d1a264d61235
Fredrik Monsen <fredrik.monsen@nb.no>
2024-02-05 07:26:12 +0100

    rewrite image_converter_tests to use input functions to reuse code

--- src-tauri/src/tests/image_converter_tests.rs
+++ src-tauri/src/tests/image_converter_tests.rs
@@ -1,10 +1,6 @@
-use std::error;
-/**
- * TempDir can not be extracted to a function, because any files/folders created are deleted when
- * TempDir goes out of scope.
- */
 use std::ffi::OsStr;
 use std::fs;
+use std::path::PathBuf;
 
 use ::tempfile::TempDir;
 
@@ -12,53 +8,46 @@ use crate::image_converter::*;
 use crate::tests::test_utils::get_test_resource_dir;
 use crate::tests::test_utils::TEST_IMAGE_PNG;
 
-#[test]
-fn test_convert_to_webp_creates_webp_of_original_in_thumbnails_directory(
-) -> Result<(), Box<dyn error::Error>> {
+fn setup_temp_dir<F: FnMut(PathBuf)>(mut handler: F) {
 	let input_image_path = get_test_resource_dir().join(TEST_IMAGE_PNG);
 
-	let tmp_dir = TempDir::with_prefix("trokk-test-tmp-")?;
+	let tmp_dir = TempDir::with_prefix("trokk-test-tmp-").expect("Failed to create temp dir");
 	let tmp_img_path = tmp_dir.path().join(TEST_IMAGE_PNG);
 
-	fs::copy(&input_image_path, &tmp_img_path)?;
+	fs::copy(&input_image_path, &tmp_img_path).expect("Failed to copy test image to temp dir");
 
-	let output_image_path = convert_to_webp(tmp_img_path)?;
+	handler(tmp_img_path);
+}
 
-	assert!(output_image_path.exists());
-	assert!(output_image_path.is_file());
-	assert_eq!(output_image_path.extension().unwrap(), OsStr::new("webp"));
-	Ok(())
+#[test]
+fn test_convert_to_webp_creates_webp_of_original_in_thumbnails_directory(
+) {
+	setup_temp_dir(|tmp_img_path| {
+		let output_image_path = convert_to_webp(tmp_img_path).unwrap();
+
+		assert!(output_image_path.exists());
+		assert!(output_image_path.is_file());
+		assert_eq!(output_image_path.extension().unwrap(), OsStr::new("webp"));
+	});
 }
 
 #[test]
 fn test_check_if_webp_exists_returns_false_if_webp_doesnt_exist(
-) -> Result<(), Box<dyn error::Error>> {
-	let input_image_path = get_test_resource_dir().join(TEST_IMAGE_PNG);
+) {
+	setup_temp_dir(|tmp_img_path| {
+		let webp_exists = check_if_webp_exists(tmp_img_path).unwrap();
 
-	let tmp_dir = TempDir::with_prefix("trokk-test-tmp-")?;
-	let tmp_img_path = tmp_dir.path().join(TEST_IMAGE_PNG);
-
-	fs::copy(&input_image_path, &tmp_img_path)?;
-
-	let webp_exists = check_if_webp_exists(tmp_img_path).unwrap();
-
-	assert!(!webp_exists);
-	Ok(())
+		assert!(!webp_exists);
+	});
 }
 
 #[test]
-fn test_check_if_webp_exists_returns_true_if_webp_exists() -> Result<(), Box<dyn error::Error>> {
-	let input_image_path = get_test_resource_dir().join(TEST_IMAGE_PNG);
+fn test_check_if_webp_exists_returns_true_if_webp_exists() {
+	setup_temp_dir(|tmp_img_path| {
+		convert_to_webp(tmp_img_path.clone()).unwrap();
 
-	let tmp_dir = TempDir::with_prefix("trokk-test-tmp-")?;
-	let tmp_img_path = tmp_dir.path().join(TEST_IMAGE_PNG);
+		let webp_exists = check_if_webp_exists(tmp_img_path).unwrap();
 
-	fs::copy(&input_image_path, &tmp_img_path)?;
-
-	convert_to_webp(tmp_img_path.clone())?;
-
-	let webp_exists = check_if_webp_exists(tmp_img_path).unwrap();
-
-	assert!(webp_exists);
-	Ok(())
+		assert!(webp_exists);
+	});
 }
