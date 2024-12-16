c8d95ad458e32375969f583b7df48fd0fc90453a
Fredrik Monsen <fredrik.monsen@nb.no>
2024-12-16 15:05:38 +0100

    feat/refactor: create thumbnail component. reset detailed image src on file tree change

--- src/features/detailed-image-view/detailed-image-view.tsx
+++ src/features/detailed-image-view/detailed-image-view.tsx
@@ -1,22 +1,15 @@
-import React, { useEffect } from 'react';
-import { X } from 'lucide-react';
+import React, {useEffect} from 'react';
 
 interface DetailedImageViewProps {
     imageSrc?: string;
+    displayTitle: boolean;
     onClose: () => void;
 }
 
-export default function DetailedImageView(props: DetailedImageViewProps) {
-
-    // Handle exit button click event
-    const handleExit = () => {
-        props.onClose();
-    }
-
-    // On Escape key press, close the detailed image view
+export default function DetailedImageView({imageSrc, displayTitle, onClose}: DetailedImageViewProps) {
     const handleKeyPress = (event: KeyboardEvent) => {
         if (event.key === 'Escape') {
-            props.onClose();
+            onClose();
         }
     }
 
@@ -29,13 +22,14 @@ export default function DetailedImageView(props: DetailedImageViewProps) {
 
     return (
         <div className="relative bg-gray-200 bg-opacity-25 dark:bg-gray-700 dark:bg-opacity-25">
+            {displayTitle && <p className="text-xl text-center py-4">{imageSrc?.split('%2F').pop() ?? ''}</p>}
             <button
-                onClick={handleExit}
+                onClick={onClose}
                 className="absolute top-0 right-0 m-4 w-10 h-10 flex items-center justify-center rounded-full bg-gray-800 text-white"
             >
                 x
             </button>
-            <img src={props.imageSrc} alt="Image in full size" className="max-h-screen w-full object-contain" />
+            <img src={imageSrc} alt="Image in full size" className="p-2.5 max-h-screen w-full object-contain" />
         </div>
     );
 }
--- src/features/files-container/files-container.tsx
+++ src/features/files-container/files-container.tsx
@@ -1,105 +1,66 @@
-import React from 'react';
-import { convertFileSrc } from '@tauri-apps/api/core';
-import { File, Folder } from 'lucide-react';
-import { formatFileNames } from '../../util/file-utils';
-import { useTrokkFiles } from '../../context/trokk-files-context.tsx';
-import { FileTree } from '../../model/file-tree.ts';
-import { sep } from '@tauri-apps/api/path';
+import React, {useEffect} from 'react';
+import {convertFileSrc} from '@tauri-apps/api/core';
+import {Folder} from 'lucide-react';
+import {useTrokkFiles} from '../../context/trokk-files-context.tsx';
 import DetailedImageView from '../detailed-image-view/detailed-image-view.tsx';
-
-const supportedFileTypes = ['jpeg', 'jpg', 'png', 'gif', 'webp'];
+import Thumbnail from '../thumbnail/thumbnail.tsx';
+import {FileTree} from '../../model/file-tree.ts';
 
 const FilesContainer: React.FC = () => {
     const { state, dispatch } = useTrokkFiles();
     const [selectedImgSrc, setselectedImgSrc] = React.useState<string | undefined>(undefined);
 
-    const getFileExtension = (path: string) => path?.split('.')?.pop() || '';
-
-    /*  Thumbnail directories are generated where '.tif' files are located.
-     *  Used to find corresponding '.webp' thumbnail for a tif, for example:
-     *
-     *  folder
-     *  ├── image_1.tif
-     *  ├── image_2.tif
-     *  ├── image_3.tif
-     *  └── .thumbnails
-     *      ├── image_1.webp
-     *      ├── image_2.webp
-     *      └── image_3.webp
-     */
-    function getThumbnailFromTree(tree: FileTree): FileTree | undefined {
-        const thumbnailPath = tree.path.substring(
-            0,
-            tree.path.length - tree.name.length
-        ) + '.thumbnails' + sep() + tree.name.split('.')[0] + '.webp';
-        return state.treeIndex.get(thumbnailPath);
-    }
-
-
-    const getThumbnailExtensionFromTree = (tree: FileTree) => {
-        const foundThumbnail = getThumbnailFromTree(tree);
-        return foundThumbnail ? getFileExtension(foundThumbnail.name) : undefined;
+    const handleDispatch = (child: FileTree) => {
+        dispatch({ type: 'SET_CURRENT_AND_EXPAND_PARENTS', payload: child });
+        setselectedImgSrc(undefined);
     };
 
-    const getThumbnailURIFromTree = (tree: FileTree) => {
-        const foundThumbnail = getThumbnailFromTree(tree);
-        return foundThumbnail ? convertFileSrc(foundThumbnail.path) : undefined;
-    };
-
-    const truncateMiddle = (str: string, frontLen: number, backLen: number) => {
-        if (str.length <= frontLen + backLen) return str;
-        return str.slice(0, frontLen) + '...' + str.slice(str.length - backLen);
-    };
+    useEffect(() => {
+        setselectedImgSrc(undefined);
+    }, [state.current]);
 
     return (
         <div className="flex flex-wrap overflow-y-auto h-[calc(96%)] justify-start content-start ml-4">
-            {
-                selectedImgSrc && (
-                    <DetailedImageView
-                        imageSrc={selectedImgSrc}
-                        onClose={() => setselectedImgSrc(undefined)}
-                    />
-                )
-            }
+
             {state.current && state.current.children ? (
-                state.current.children.length !== 0 ? (
-                    state.current.children.map((child) => (
-                        !child.name.startsWith('.thumbnails') && child.isDirectory ? (
-                            <button
-                                key={child.path}
-                                className="max-w-[150px]"
-                                onClick={() => dispatch({ type: 'SET_CURRENT_AND_EXPAND_PARENTS', payload: child })}
-                            >
-                                <Folder size="96" />
-                                <i>{child.name}</i>
-                            </button>
-                        ) : (
-                            supportedFileTypes.includes(getFileExtension(child.path)) ? (
-                                <div key={child.path} className="border-2 border-stone-500 max-w-[150px] mr-2 mb-2"
-                                     onClick={() => setselectedImgSrc(convertFileSrc(child.path))}>
-                                    <img src={convertFileSrc(child.path)} alt={child.name}/>
-                                    <i>{formatFileNames(child.name)}</i>
-                                </div>
-                            ) : getThumbnailExtensionFromTree(child) === 'webp' ? (
-                                <div key={child.path} className="border-2 border-stone-500 max-w-[150px] mr-2 mb-2"
-                                     onClick={() => setselectedImgSrc(getThumbnailURIFromTree(child))}>
-                                    <img src={getThumbnailURIFromTree(child)} alt={child.name}/>
-                                    <i>{truncateMiddle(formatFileNames(child.name), 7, 10)}</i>
-                                </div>
-                            ) : child.name !== '.thumbnails' && (
-                                <div key={child.path} className="max-w-[150px] mr-2 mb-2 flex flex-col items-center"
-                                     onClick={() => setselectedImgSrc(undefined)}>
-                                    <File size="96" color="gray"/>
-                                    <i>{truncateMiddle(child.name, 7, 10)}</i>
-                                </div>
-                            )
+                <>
+                    {
+                        selectedImgSrc && (
+                            <DetailedImageView
+                                imageSrc={selectedImgSrc}
+                                displayTitle={true}
+                                onClose={() => setselectedImgSrc(undefined)}
+                            />
                         )
-                    ))
-                ) : (
-                    <p className="m-8 font-bold break-words">
-                        Ingen filer i mappen.
-                    </p>
-                )
+                    }
+                    {
+                        state.current.children.length !== 0 ? (
+                            state.current.children.map((child) => (
+                                !child.name.startsWith('.thumbnails') && child.isDirectory ? (
+                                    <button
+                                        key={child.path}
+                                        className="max-w-[150px]"
+                                        onClick={() => handleDispatch(child)}
+                                    >
+                                        <Folder size="96" />
+                                        <i>{child.name}</i>
+                                    </button>
+                                ) : (
+                                    <Thumbnail
+                                        key={child.name}
+                                        onClick={() => setselectedImgSrc(convertFileSrc(child.path))}
+                                        fileTree={child}
+                                    />
+                                )
+                            ))
+                        ) : (
+                            <p className="m-8 font-bold break-words">
+                                Ingen filer i mappen.
+                            </p>
+                        )
+                    }
+                </>
+
             ) : (
                 <p className="m-8 font-bold break-words">
                     Velg en mappe i listen til venstre. <br />
