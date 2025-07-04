2dccf3555b438a56e3cf5f9580ea25f3d1f366fd
Ida-Sofie <ida.langstrand@gmail.com>
2025-07-04 12:43:00 +0200

    feat: putting preview in front of page, adding close functionality outside etc.

--- src/features/detailed-image-view/detailed-image-view.tsx
+++ src/features/detailed-image-view/detailed-image-view.tsx
@@ -5,16 +5,19 @@ import {getPreviewURIFromTree} from '../../util/file-utils.ts';
 import {useTrokkFiles} from '../../context/trokk-files-context.tsx';
 import LoadingSpinner from '../../components/ui/loading-spinner.tsx';
 import {useSelection} from '../../context/selection-context.tsx';
+import Checkbox from '../../components/ui/checkbox.tsx';
 
 export interface DetailedImageViewProps {
     image: FileTree;
     totalImagesInFolder: number;
+    isChecked: boolean;
 }
 
-export default function DetailedImageView({ image, totalImagesInFolder}: DetailedImageViewProps) {
+export default function DetailedImageView({image, totalImagesInFolder, isChecked}: DetailedImageViewProps) {
     const {state, dispatch} = useTrokkFiles();
     const [isLoading, setIsLoading] = useState<boolean>(false);
-    const { currentIndex, handleNext, handlePrevious, handleClose  } = useSelection()
+    const {currentIndex, handleNext, handlePrevious, handleClose, handleCheck} = useSelection();
+
     useEffect(() => {
         dispatch({type: 'UPDATE_PREVIEW', payload: image});
     }, []);
@@ -34,30 +37,52 @@ export default function DetailedImageView({ image, totalImagesInFolder}: Detaile
     };
 
     return (
-        <div className="relative">
-            <p className="text-center pt-4">Viser bilde {currentIndex + 1} av {totalImagesInFolder}</p>
-            <div className="flex justify-center mt-4">
-                <button onClick={handlePrevious} className="mx-2 px-4 py-2 bg-gray-800 text-white rounded">
-                    <ChevronLeft/>
-                </button>
-                <button onClick={handleNext} className="mx-2 px-4 py-2 bg-gray-800 text-white rounded">
-                    <ChevronRight/>
-                </button>
-            </div>
-            <button
-                onClick={handleClose}
-                className="absolute top-[100px] right-0 m-4 w-10 h-10 flex items-center justify-center rounded-full bg-gray-800 text-white"
-            >
-                x
-            </button>
+        <div className={'relative z-10'} onClick={handleClose}>
             {isLoading ? (
                 <div className="flex justify-center my-8 ">
                     <LoadingSpinner size={48}/>
                 </div>
 
             ) : (
-                <img src={getImageSrc()} alt="Forhåndsvisning av bilde"
-                     className="p-2.5 max-h-screen w-full object-contain"/>
+                <div className="flex justify-center items-start pt-5" >
+                    <button
+                            className="shadow-2xl h-20 px-6 bg-stone-200 hover:bg-stone-300 text-stone-600 rounded self-center"
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                handlePrevious();
+                            }}
+                    >
+                        <ChevronLeft/>
+                    </button>
+                    <div className="px-6" onClick={(e) => e.stopPropagation()}>
+                        <div className="flex flex-col justify-center pt-4 bg-stone-300 rounded-t-xl">
+                            <div className="flex flex-row items-start justify-center" >
+                                <p className ="text-black text-2xl ml-10 ">{isChecked ? 'Forside valgt': 'Velg forside'}</p>
+                                <Checkbox
+                                    isChecked={isChecked}
+                                    onChange={() => handleCheck()}
+                                    isFocused={false}
+                                />
+                            </div>
+                            <p className="text-center text-lg text-stone-700">Viser bilde {currentIndex + 1} av {totalImagesInFolder}</p>
+                        </div>
+                        <img
+                            src={getImageSrc()}
+                            alt="Forhåndsvisning av bilde"
+                            className={`shadow-2xl max-h-[calc(100vh-110px)] object-contain ${isChecked ? 'border-amber-400 border-8' : ''}`}
+                        />
+                    </div>
+
+                    <button
+                        className="shadow-2xl h-20 px-6 py-2 bg-stone-200 hover:bg-stone-300 text-stone-600 rounded self-center"
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            handleNext();
+                        }}
+                    >
+                        <ChevronRight/>
+                    </button>
+                </div>
             )}
         </div>
     );
