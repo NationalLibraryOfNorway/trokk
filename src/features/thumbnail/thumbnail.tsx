8603f2ba27e872cdb507aee4fd8abff2cbf61145
Ida-Sofie <ida.langstrand@gmail.com>
2025-07-04 12:45:23 +0200

    fix/feat: restructured component, changed ux

--- src/features/thumbnail/thumbnail.tsx
+++ src/features/thumbnail/thumbnail.tsx
@@ -5,10 +5,10 @@ import {
     getThumbnailURIFromTree,
     supportedFileTypes
 } from '../../util/file-utils.ts';
-import {FileTree} from '../../model/file-tree.ts';
-import {convertFileSrc} from '@tauri-apps/api/core';
-import {File} from 'lucide-react';
-import {useTrokkFiles} from '../../context/trokk-files-context.tsx';
+import { FileTree } from '../../model/file-tree.ts';
+import { convertFileSrc } from '@tauri-apps/api/core';
+import { File } from 'lucide-react';
+import { useTrokkFiles } from '../../context/trokk-files-context.tsx';
 
 export interface ThumbnailProps {
     fileTree: FileTree;
@@ -17,39 +17,42 @@ export interface ThumbnailProps {
     isFocused: boolean;
 }
 
-
-export default function Thumbnail({fileTree, onClick, isChecked, isFocused}: ThumbnailProps) {
-    const {state} = useTrokkFiles();
+export default function Thumbnail({ fileTree, onClick, isChecked, isFocused }: ThumbnailProps) {
+    const { state } = useTrokkFiles();
 
     const truncateMiddle = (str: string, frontLen: number, backLen: number) => {
         if (str.length <= frontLen + backLen) return str;
         return str.slice(0, frontLen) + '...' + str.slice(str.length - backLen);
     };
 
-    return (
-        supportedFileTypes.includes(getFileExtension(fileTree?.path)) ? (
-            <div key={fileTree.path}
-                 className={`border-2 rounded-sm max-w-[150px] mr-2 mb-2 hover:bg-gray-300 hover:dark:bg-gray-600 ${isChecked ? 'border-amber-400' : 'border-gray-300 dark:border-gray-600'} ${isFocused ? 'ring-2 ring-white' : ''}
-    `}
-                 onClick={onClick}>
-                <img src={convertFileSrc(fileTree.path)} alt={fileTree.name}/>
-                <i>{truncateMiddle(formatFileNames(fileTree.name), 7, 10)}</i>
-            </div>
-        ) : getThumbnailExtensionFromTree(fileTree, state) === 'webp' ? (
-            <div key={fileTree.path}
-                 className={`border-2   max-w-[150px] mr-2 mb-2 hover:bg-gray-300 hover:dark:bg-gray-600 ${isChecked ? 'border-amber-400' : 'border-gray-300 dark:border-gray-600'} ${isFocused ? 'ring-2 ring-white' : ''}
-            `}
-                 onClick={onClick}>
-                <img src={getThumbnailURIFromTree(fileTree, state)} alt={fileTree.name}/>
-                <i>{truncateMiddle(formatFileNames(fileTree.name), 7, 10)}</i>
-            </div>
-        ) : !(fileTree.name === '.thumbnails' || fileTree.name === '.previews') && (
-            <div key={fileTree.path} className="max-w-[150px] mr-2 mb-2 flex flex-col items-center"
-                 onClick={onClick}>
-                <File size="96" color="gray"/>
-                <i>{truncateMiddle(fileTree.name, 7, 10)}</i>
-            </div>
-        )
-    );
+    const fileName = truncateMiddle(formatFileNames(fileTree.name), 7, 10);
+    const isSupported = supportedFileTypes.includes(getFileExtension(fileTree?.path));
+    const isWebp = getThumbnailExtensionFromTree(fileTree, state) === 'webp';
+    const isHiddenDir = fileTree.name === '.thumbnails' || fileTree.name === '.previews';
 
+    if (isHiddenDir) return null;
+
+    let className = '';
+
+    if (isChecked) {
+        className += `${isFocused ? ' ring-4 ring-blue-600' : ''} border-8 border-amber-400 hover:border-amber-300 `;
+    } else if (isFocused) {
+        className += 'ring-4 ring-blue-600 hover:ring-blue-500';
+    }
+
+    let content: React.ReactNode;
+    if (isSupported) {
+        content = <img className={`${className} w-full`} src={convertFileSrc(fileTree.path)} alt={fileTree.name} />;
+    } else if (isWebp) {
+        content = <img className={`${className} w-full`} src={getThumbnailURIFromTree(fileTree, state)} alt={fileTree.name} />;
+    } else {
+        content = <File size="96" color="gray" />;
+    }
+
+    return (
+        <div key={fileTree.path} className="flex flex-col items-center justify-center hover:bg-stone-700" onClick={onClick}>
+            {content}
+            <i className={`flex content-center justify-center text-lg ${isChecked? 'text-amber-400' : ''}`}>{fileName}</i>
+        </div>
+    );
 }
