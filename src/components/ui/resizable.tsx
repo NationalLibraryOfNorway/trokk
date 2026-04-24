ff663a77af0c071f2a2598b4fd3b184b098ad1b0
Gard Marius Guldhav <gard.guldhav@nb.no>
2026-04-24 13:41:59 +0200

    feat: revert to vanilla Shadcn

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,61 +1,43 @@
-import * as React from 'react';
-import {GripVertical} from 'lucide-react';
-import {
-    Group,
-    Panel,
-    type PanelProps,
-    Separator,
-    type GroupProps,
-    type SeparatorProps,
-} from 'react-resizable-panels';
-import {cn} from '@/lib/utils.ts';
+import { GripVertical } from "lucide-react"
+import { Group as PanelGroup, Panel, Separator as PanelResizeHandle } from "react-resizable-panels"
 
-const ResizablePanelGroup = React.forwardRef<
-    HTMLDivElement,
-    GroupProps
->(({className, ...props}, ref) => (
-    <Group
-        elementRef={ref}
-        className={cn(
-            'flex h-full w-full data-[orientation=vertical]:flex-col',
-            className,
-        )}
-        {...props}
-    />
-));
-ResizablePanelGroup.displayName = 'ResizablePanelGroup';
+import { cn } from "@/lib/utils"
 
-const ResizablePanel = React.forwardRef<
-    HTMLDivElement,
-    PanelProps
->(({className, ...props}, ref) => (
-    <Panel
-        elementRef={ref}
-        className={cn('min-w-0 min-h-0', className)}
-        {...props}
-    />
-));
-ResizablePanel.displayName = 'ResizablePanel';
+const ResizablePanelGroup = ({
+  className,
+  ...props
+}: React.ComponentProps<typeof PanelGroup>) => (
+  <PanelGroup
+    className={cn(
+      "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
+      className
+    )}
+    {...props}
+  />
+)
 
-const ResizableHandle = React.forwardRef<
-    HTMLDivElement,
-    SeparatorProps & {withHandle?: boolean}
->(({className, withHandle, ...props}, ref) => (
-    <Separator
-        elementRef={ref}
-        className={cn(
-            'relative flex w-2 items-center justify-center bg-stone-900/80 transition-colors after:absolute after:inset-y-0 after:left-1/2 after:w-px after:-translate-x-1/2 after:bg-stone-700 hover:bg-stone-800/90 focus-visible:outline-none focus-visible:ring-0 data-[orientation=vertical]:h-2 data-[orientation=vertical]:w-full data-[orientation=vertical]:after:left-0 data-[orientation=vertical]:after:top-1/2 data-[orientation=vertical]:after:h-px data-[orientation=vertical]:after:w-full data-[orientation=vertical]:after:-translate-y-1/2 data-[orientation=vertical]:after:translate-x-0',
-            className,
-        )}
-        {...props}
-    >
-        {withHandle ? (
-            <div className="z-10 rounded-sm border border-stone-700 bg-stone-800 p-1 text-stone-300 shadow-sm">
-                <GripVertical className="size-3.5" />
-            </div>
-        ) : null}
-    </Separator>
-));
-ResizableHandle.displayName = 'ResizableHandle';
+const ResizablePanel = Panel
 
-export {ResizableHandle, ResizablePanel, ResizablePanelGroup};
+const ResizableHandle = ({
+  withHandle,
+  className,
+  ...props
+}: React.ComponentProps<typeof PanelResizeHandle> & {
+  withHandle?: boolean
+}) => (
+  <PanelResizeHandle
+    className={cn(
+      "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
+      className
+    )}
+    {...props}
+  >
+    {withHandle && (
+      <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-border">
+        <GripVertical className="h-2.5 w-2.5" />
+      </div>
+    )}
+  </PanelResizeHandle>
+)
+
+export { ResizablePanelGroup, ResizablePanel, ResizableHandle }
